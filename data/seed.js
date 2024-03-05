bea9b35cd57b1457ca29ec12b858488be4a20083
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-03-04 20:17:44 -0800

    Shifted placeholder data into seed file.

--- data/seed.js
+++ data/seed.js
@@ -3,8 +3,8 @@
 // ********************
 // ABOUT - For testing purposes, can be removed or commented out once database is running successfully
 
-// const books = [
-//   { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925 },
-//   { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', publicationYear: 1960 },
-//   { id: 3, title: '1984', author: 'George Orwell', publicationYear: 1949 }
-// ];
+const books = [
+  { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925 },
+  { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', publicationYear: 1960 },
+  { id: 3, title: '1984', author: 'George Orwell', publicationYear: 1949 }
+];
