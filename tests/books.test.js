bc327f74e1f4f4308ae1eccadb357271acc92ad0
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-03-04 22:10:17 -0800

    Added basic tests for Status 200.

--- tests/books.test.js
+++ tests/books.test.js
@@ -5,52 +5,62 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('Testing API Endpoints', () => {
+    let bookID = 1;
 
     // 1. Test GET /books: 
     describe('GET /books', () => {
         it('should retrieve a list of all books. Implement pagination to limit the number of books returned per request.', async () => {
-            expect();
+            const response = await request(app).get('/books');
+            expect(response.status).to.equal(200);
         });
     });
     // 2. Test GET /books/{id}: 
     describe('GET /books/:id', () => {
         it('should retrieve details of a specific book by ID', async () => {
-            expect();
+            const response = await request(app).get(`/books${bookID}`)
+            expect(response.status).to.equal(200);
         });
     });
 
     // 3. Test POST /books: 
     describe('POST /books', () => {
         it('should add a new book to the collection, and use input validation to ensure all required fields are provided (`title`, `author`, `publicationYear`), and `publicationYear` should be a valid year in the past.', async () => {
-            expect();
+            const response = await request(app).post('/books');
+            expect(response.status).to.equal(200);
         });
     });
 
     // 4. Test PUT /books/{id}: 
     describe('PUT /books/:id', () => {
         it('should update details of a specific book by ID, and should partial updates, and ensure validation is applied to the input data.', async () => {
-            expect();
+            const response = request(app).put(`/books/${bookID}`);
+            expect(response.status).to.equal(200);
+
         });
     });
 
     // 5. Test DELETE /books/{id}: 
     describe('DELETE /books/:id', () => {
         it('should delete a specific book by ID', async () => {
-            expect();
+            const response = request(app).delete(`/books/${bookID}`)
+            expect(request.status).to.equal(200);
         });
     });
 
     // 6. Test GET /books/search?q={query}: 
     describe('GET /books/search', () => {
         it('should allow users to search for books by title or author', async () => {
-            expect();
+            const query = 1925;
+            const response = request(app).get(`books/search?q=${query}`)
+            expect(request.status).to.equal(200);
         });
     });
 
     // 7. Test GET /books/stats: 
     describe('GET /books/stats', () => {
         it('should provide statistics about the collection of books, including the total number of books, the earliest and latest publication years, and any other relevant metrics you think are appropriate.', async () => {
-            expect();
+            const response = request(app).get('/books/stats');
+            expect(request.status).to.equal(200);
         });
     });
 
